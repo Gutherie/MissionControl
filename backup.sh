f79aba49d7f4ff64cf768666344deed91b879b37
Jason R. Baker <jasonbaker@ieee.org>
2015-07-14 15:51:59 -0400

    Backup file edited

--- backup.sh
+++ backup.sh
@@ -5,9 +5,8 @@
 ###  system and delete                               ##
 #####################################################
 
-
-#BK_SRV=zope2.fammedmcmaster.ca
-BASE_DIR=/srv/Plone/plone-main/zinstance/var
+EMAIL="youremail@here.com"
+BASE_DIR=/srv/plone-service/plone4-prod/var
 TMP_DIR=/tmp/BACKUP
 DATA=/data
 FILE=/filestorage
@@ -15,64 +14,79 @@ BLOB=/blobstorage
 STATUS="Backup was not completed for "
 SITES=(main bayswatermd hchlink mat_centre oscarResource tapestry chap iampreg mfp oscarResource-plone trihpp dfmh iam_x my_blood_pressure oscartools ebm-chews inch oscarcanada qcanz fht it oscarmanual quality fmah macfm oscar-resource sfhc)
 
+# arg1 : tmpdir
+# arg2 : datadir
+# arg3 : blobdir
+# arg4 : appname
+# arg5 : servername
+ 
+function process {
+	echo "Archiving data file for $4"
+        ssh $5 "tar -pcvzf ${1}/Data.fs-$4.tar.gz $2/Data.fs"
+	sleep 5
+        echo "Archiving blobstorage for $2"
+        ssh $5 "tar -pcvzf $TMP_DIR/blobstorage-$4.tar.gz $3"
+	sleep 5
+        echo "send data to backup server" 
+        scp $5:$1/Data.fs-$4.tar.gz ./
+	sleep 5
+        ssh $5 "rm $TMP_DIR/Data.fs-$4.tar.gz"
+	sleep 5
+        echo "send blobs to backup"
+        scp $5:$1/blobstorage-$4.tar.gz ./
+	sleep 5
+        ssh $5 "rm $TMP_DIR/blobstorage-$4.tar.gz"
+	sleep 5
+        STATUS="Backup was completed for "$4
+}
 
 if [ $# -gt 0 ]
 then
 	if [ $1 == "?" ]
 	then
-		echo "Available arguments (site to backup) are :"
+		echo "Available arguments (site to backup) are servername and site (one of the following):"
 		for server in ${SITES[*]}
 		do
 			echo $server
 		done
 		echo "  ... or ? for help"
-	else
-		# special case for main, others are located in data
-		if [ $1 == ${SITES[0]} ]
+	else 
+		if [ $# -eq 1 ]
 		then
-			echo "Archiving data file for $1"
-			cd ${BASE_DIR}${FILE}
-			tar -pcvzf $TMP_DIR/Data.fs-$1.tar.gz Data.fs
+			echo "Processing all sites, enter any key to continue (ctrl-C to cancel)"
+			read
 
-			echo "Archiving blobstorage for $1"
-			cd ${BASE_DIR}
-			tar -pcvzf $TMP_DIR/blobstorage-$1.tar.gz .${BLOB}
-
- 			# send data to backup 
- 			#scp $TMP_DIR/Data.fs-$1.tar.gz ${BK_SRV}:/srv/BACKUP
-                        #rm $TMP_DIR/Data.fs-$1.tar.gz
-
-			# send blobs to backup
-                        #scp $TMP_DIR/blobstorage-$1.tar.gz ${BK_SRV}:/srv/BACKUP
-                        #rm $TMP_DIR/blobstorage-$1.tar.gz
-			STATUS="Backup was completed for "$1
-		else
 			for server in ${SITES[*]}
 			do
-				if [ $server == $1 ]
+				echo "Processing a single site (${server}), enter any key to continue (ctrl-c to cancel)"
+                                if [ ${server} == ${SITES[0]} ]
+                                then
+                                        process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} ${server} $1
+                                else
+                                        process ${TMP_DIR} ${BASE_DIR}${DATA}/${server}/${FILE} ${BASE_DIR}${DATA}/${server}${BLOB} ${server} $1
+                                fi
+			done	
+			
+		else 
+			if [ $# -eq 2 ]
+			then
+				echo "Processing a single site ($2), enter any key to continue (ctrl-c to cancel)"
+				read
+				if [ $2 == ${SITES[0]} ]
 				then
-		                        echo "Archiving data file for $1"
-		                        cd ${BASE_DIR}${DATA}/$1${FILE}
-		                        tar -pcvzf $TMP_DIR/Data.fs-$1.tar.gz Data.fs
-
-					echo "Archiving blobstorage for $1"
-			                cd ${BASE_DIR}${DATA}/$1
-			                tar -pcvzf $TMP_DIR/blobstorage-$1.tar.gz .${BLOB}
-
-					# send data to backup
-                                        scp $TMP_DIR/Data.fs-$1.tar.gz ${BK_SRV}:/srv/BACKUP
-	                                rm $TMP_DIR/Data.fs-$1.tar.gz
-
-					# send blobs to backup
-					scp $TMP_DIR/blobstorage-$1.tar.gz ${BK_SRV}:/srv/BACKUP
-					rm $TMP_DIR/blobstorage-$1.tar.gz
-					
-					STATUS="Backup was completed for "$1
+					process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} $2 $1
+				else
+					process ${TMP_DIR} ${BASE_DIR}${DATA}/$2/${FILE} ${BASE_DIR}${DATA}/$2${BLOB} $2 $1
 				fi
-			done
+			else
+				echo "Error with your selection, only two arguements allowed"
+			fi
 		fi
 	fi
-	echo ${STATUS} | /usr/bin/mailx -s "zope1 backup" jasonbaker@ieee.org
+	# echo ${STATUS} | /usr/bin/mailx -s "zope1 backup" $EMAIL
 else
 	echo "You must enter on argument: ? for help"
 fi
+
+echo "Processing complete! Have a nice day"
+exit 0
