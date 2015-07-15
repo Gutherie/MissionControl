4b2c326b25b49cc64b6035f6280eab7192f15bc9
Jason R. Baker <jasonbaker@ieee.org>
2015-07-15 13:40:39 -0400

    Added a date prefix to the file naming

--- backup.sh
+++ backup.sh
@@ -13,30 +13,34 @@ FILE=/filestorage
 BLOB=/blobstorage
 STATUS="Backup was not completed for "
 SITES=(main bayswatermd hchlink mat_centre oscarResource tapestry chap iampreg mfp oscarResource-plone trihpp dfmh iam_x my_blood_pressure oscartools ebm-chews inch oscarcanada qcanz fht it oscarmanual quality fmah macfm oscar-resource sfhc)
+DELAY=30
+
+DATE=`date +%Y-%m-%d`
 
 # arg1 : tmpdir
 # arg2 : datadir
 # arg3 : blobdir
 # arg4 : appname
 # arg5 : servername
+# arg6 : date prefix
  
 function process {
 	echo "Archiving data file for $4"
-        ssh $5 "tar -pcvzf ${1}/Data.fs-$4.tar.gz $2/Data.fs"
-	sleep 5
+        ssh $5 "tar -pcvzf ${1}/${6}_Data.fs-$4.tar.gz $2/Data.fs"
+	sleep ${DELAY}
         echo "Archiving blobstorage for $2"
-        ssh $5 "tar -pcvzf $TMP_DIR/blobstorage-$4.tar.gz $3"
-	sleep 5
+        ssh $5 "tar -pcvzf $TMP_DIR/${6}_blobstorage-$4.tar.gz $3"
+	sleep ${DELAY}
         echo "send data to backup server" 
-        scp $5:$1/Data.fs-$4.tar.gz ./
-	sleep 5
-        ssh $5 "rm $TMP_DIR/Data.fs-$4.tar.gz"
-	sleep 5
+        scp $5:$1/${6}_Data.fs-$4.tar.gz ./
+	sleep ${DELAY}
+        ssh $5 "rm $TMP_DIR/${6}_Data.fs-$4.tar.gz"
+	sleep ${DELAY}
         echo "send blobs to backup"
-        scp $5:$1/blobstorage-$4.tar.gz ./
-	sleep 5
-        ssh $5 "rm $TMP_DIR/blobstorage-$4.tar.gz"
-	sleep 5
+        scp $5:$1/${6}_blobstorage-$4.tar.gz ./
+	sleep ${DELAY}
+        ssh $5 "rm $TMP_DIR/${6}_blobstorage-$4.tar.gz"
+	sleep ${DELAY}
         STATUS="Backup was completed for "$4
 }
 
@@ -61,10 +65,11 @@ then
 				echo "Processing a single site (${server}), enter any key to continue (ctrl-c to cancel)"
                                 if [ ${server} == ${SITES[0]} ]
                                 then
-                                        process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} ${server} $1
+                                        process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} ${server} $1 ${DATE}
                                 else
-                                        process ${TMP_DIR} ${BASE_DIR}${DATA}/${server}${FILE} ${BASE_DIR}${DATA}/${server}${BLOB} ${server} $1
+                                        process ${TMP_DIR} ${BASE_DIR}${DATA}/${server}${FILE} ${BASE_DIR}${DATA}/${server}${BLOB} ${server} $1 ${DATE}
                                 fi
+				sleep ${DELAY} 
 			done	
 			
 		else 
@@ -74,9 +79,9 @@ then
 				read
 				if [ $2 == ${SITES[0]} ]
 				then
-					process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} $2 $1
+					process ${TMP_DIR} ${BASE_DIR}${FILE} ${BASE_DIR}${BLOB} $2 $1 ${DATE}
 				else
-					process ${TMP_DIR} ${BASE_DIR}${DATA}/$2${FILE} ${BASE_DIR}${DATA}/$2${BLOB} $2 $1
+					process ${TMP_DIR} ${BASE_DIR}${DATA}/$2${FILE} ${BASE_DIR}${DATA}/$2${BLOB} $2 $1 ${DATE}
 				fi
 			else
 				echo "Error with your selection, only two arguements allowed"
